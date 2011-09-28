1950a26f73a3084365e81ca0a7230b710fdcd0fb
Cavell Blood <me@cavellblood.com>
2011-09-28 10:24:32 -0700

    Edit this embed

---
+++ template-files/includes/_edit-this.php
@@ -0,0 +1,1 @@
+{if logged_in && (group_id == "6" || group_id == "1")}<a href="{cp_url}?&D=cp&C=content_publish&M=entry_form&channel_id={embed:channel}&entry_id={embed:entry}" target="_blank">Edit This</a>{/if}
